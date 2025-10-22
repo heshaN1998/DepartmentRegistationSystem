67747daec42bac32558b40c747b2dc695bb3f91b
heshan <heshansandakelum2018@gmail.com>
2025-10-22 10:23:52 +0530

    cruds & updates

--- src/main/java/lk/mas/kreeda_1987/departmentRegisterPro/controller/EmployController.java
+++ src/main/java/lk/mas/kreeda_1987/departmentRegisterPro/controller/EmployController.java
@@ -1,11 +1,83 @@
 package lk.mas.kreeda_1987.departmentRegisterPro.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.mas.kreeda_1987.departmentRegisterPro.dto.Role;
+import lk.mas.kreeda_1987.departmentRegisterPro.dto.UserDTO;
+import org.apache.catalina.filters.ExpiresFilter;
+import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/employ")
 public class EmployController {
+//Todo: Handle employ detail as CRUD OP
+
+    //save employ
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<UserDTO> saveEmploy(@RequestBody UserDTO userDTO) {
+        return new ResponseEntity<>(userDTO, HttpStatus.CREATED);
+
+    }
+
+    @GetMapping(value = "{employId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<UserDTO> getSelectedEmploy(@PathVariable String employId) {
+        System.out.println("Employ ID is" + employId);
+        var userDTO = new UserDTO(userId:"stu1", firstName:"jv,", lastName = "do", email:"sda", addressLine1:
+        "a", addressLine2:"b", addressLine3:"c", city:"sdasd", password:"dawdad", Role.EMPLOY);
+        if (employId.equals(userDTO.getUserId())) {
+            return new ResponseEntity<>(userDTO, HttpStatus.OK);
+
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<UserDTO>> getAllEmploy() {
+        List<userDTO> employList = Arrays.asList(
+                new UserDTO(
+                        userId:"U003",
+                firstName:"Clara",
+                lastName:"Nguyen",
+                email:"clara.nguyen@example.com",
+                addressLine1:"789 Pine Road",
+                addressLine2:"Suite 210",
+                addressLine3:"das",
+                city:"Chicago",
+                password:"clara@2025",
+                Role.EMPLOY),
+        new UserDTO(
+                userId:"U002",
+                firstName:"SAS",
+                lastName:"Martinez",
+                email:"bob.martinez@example.com",
+                addressLine1:"456 Oak Avenue",
+                addressLine2:"sas",
+                addressLine3:"s",
+                city:"Los Angeles",
+                password:"bobSecure!45",
+                Role.EMPLOY)
+
+
+
+       );
+        return new ResponseEntity<>(employList, HttpStatus.OK)
+    }
+        @PatchMapping
+            public void updateEmploy(@RequestParam("stuId") String employ,@RequestParam  String stuCity,@RequestBody UserDTO tobeUpdateDetails ){
+            System.out.println("employ id is" + studentId);
+            System.out.println("employ city" + stuCity);
+            System.out.println("updated" + tobeUpdateDetails);
+        }
+
+    ;
+}
+}
+
+
 
 }
--- src/main/java/lk/mas/kreeda_1987/departmentRegisterPro/dto/UserDTO.java
+++ src/main/java/lk/mas/kreeda_1987/departmentRegisterPro/dto/UserDTO.java
@@ -4,19 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.management.relation.Role;
 import java.io.Serializable;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+
 public class UserDTO implements Serializable {
     private String userId;
     private String firstName;
     private String lastName;
     private String email;
     private String addressLine1;
-    private String getAddressLine2;
-    private String getAddressLine3;
+    private String addressLine2;
+    private String addressLine3;
     private String city;
     private String password;
     private Role role;
